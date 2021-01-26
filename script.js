097f0753646f4f19dbc9b60f4f9f76f9b86dbdca
Rebecca Ross <b.laurenross@gmail.com>
2021-01-26 23:03:40 +0000

    Gets info from API

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const cases = document.querySelector('.cases');
+const deaths = document.querySelector('.deaths');
+const recovered = document.querySelector('.recovered');
+const affectedCountries = document.querySelector('.affected-countries');
 
 fetch('https://disease.sh/v3/covid-19/all')
   .then(res => res.json())
@@ -6,4 +9,13 @@ fetch('https://disease.sh/v3/covid-19/all')
     console.log(data)
     const casesToday = data.cases;
     cases.innerHTML = casesToday;
+
+    const death = data.deaths;
+    deaths.innerHTML = death;
+
+    const recover = data.recovered;
+    recovered.innerHTML = recover;
+
+    const affected = data.affectedCountries;
+    affectedCountries.innerHTML = affected;
   });
